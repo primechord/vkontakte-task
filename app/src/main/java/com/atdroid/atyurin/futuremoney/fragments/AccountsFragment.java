0805319bd629b9e12a0211c2620417c1c19ff075
Alexey Tyurin <alexeytyurin74@gmail.com>
2015-11-13 01:03:39 +0500

    account item deleted devider lines

--- app/src/main/java/com/atdroid/atyurin/futuremoney/fragments/AccountsFragment.java
+++ app/src/main/java/com/atdroid/atyurin/futuremoney/fragments/AccountsFragment.java
@@ -51,7 +51,7 @@ public class AccountsFragment extends Fragment {
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);//switch on menu for fragment
 
-        rootView = inflater.inflate(R.layout.fragment_budget_items_list, container, false);
+        rootView = inflater.inflate(R.layout.fragment_account_items_list, container, false);
         ListView lvAccounts = (ListView) rootView.findViewById(R.id.lv_budget_items);
         dao = new AccountsDAO(activity.getBaseContext());
         dao.openReadable();
