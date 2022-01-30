44e0bb6e1b5ae100ce8b1b13c80f1e136e45c31f
sopcastultras <gats1499@gmail.com>
2022-01-30 19:51:24 +0300

    screenshot rule restored

--- app/src/androidTest/java/com/vk/sopcastultras/futuremoney/BaseTest.kt
+++ app/src/androidTest/java/com/vk/sopcastultras/futuremoney/BaseTest.kt
@@ -5,6 +5,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.atdroid.atyurin.futuremoney.activity.MainActivity
 import io.qameta.allure.android.rules.LogcatRule
+import io.qameta.allure.android.rules.ScreenshotRule
 import io.qameta.allure.android.rules.WindowHierarchyRule
 import io.qameta.allure.android.runners.AllureAndroidJUnit4
 import org.junit.Before
@@ -14,7 +15,8 @@ import org.junit.runner.RunWith
 @RunWith(AllureAndroidJUnit4::class)
 abstract class BaseTest {
 
-    // @get:Rule val screenshotRule = ScreenshotRule(mode = ScreenshotRule.Mode.FAILURE) FIXME
+    @get:Rule
+    val screenshotRule = ScreenshotRule(mode = ScreenshotRule.Mode.FAILURE)
 
     @get:Rule
     val logcatRule = LogcatRule()
